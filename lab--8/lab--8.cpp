f84321fc6fa063ca497f5a5c6eaa45adf63fc0ee
AngelinaPrisaznuk <114239162+AngelinaPrisaznuk@users.noreply.github.com>
2023-01-10 17:53:50 +0200

    Add project files.

---
+++ lab--8/lab--8.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+#define maximum(x, y, r) \
+do { int aa = ( x ); int bb = ( y ); ( r ) = bb < aa ? aa : bb; } while ( false )
+
+int main()
+{
+    int x = -10, y = -5, r;
+
+    if (x > 0 && y > 0) maximum(x, y, r);
+    else maximum(x = -x, y = -y, r);
+
+    std::cout << "x = " << x << ", y = " << y << ", r = " << r << std::endl;
+}
